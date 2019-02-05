d048c28d87f3f858ac52d6dff657a99670cd36c0
Anna <annabaumann@laposte.net>
2019-02-06 00:26:05 +0100

    typo + content

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import "~@/assets/scss/_reset.scss";
+@import "~@/assets/scss/_fonts.scss";
+// @import "~@/assets/scss/_fonts.scss";
+// 
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Monument Extended", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  min-height: 100vh;
+  background-color: #2a472e;
+  color: #DA7A7A;
 }
 #nav {
   padding: 30px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>Hello i'm</div>
+    <h1 class="home__title">Anna Baumann,</h1>
+    <div>a front-end developer looking for a six-month intership.</div>
+    <div>
+      <div>My totems emojis are </div>
+      <div class="emojis">☀️🐬🥨</div>
+    </div>
+    <div>I had fun working on this projects</div>
+    <div> yoyoyo.photos, agathe et anna, indiscipline </div>
+    <div>You can find my cyber me on linkedin, instagram and github</div>
+    <div>I also love to experiment cool stuff on the internet :</div>
+    <div>generative design, video canvas, threes.js</div>
+    <div>I also love contemporary art, sun,  big diner with my friends, scenography, italo disco music, fashion, cooking, flowers, the blaze, blurred pictures , beers, partir en week-end, the sea
+</div>
   </div>
 </template>
 
@@ -16,3 +28,24 @@ export default {
   }
 };
 </script>
+<style scoped lang="scss">
+.home {
+  padding: 20vh 10vw;
+  font-size: 4vw;
+}
+.home__title {
+  font-size: 6vw;
+}
+.emojis {
+  position: relative;
+  .emojis__line {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    font-size: 20vh;
+  }
+  &:hover {
+
+  }
+}
+</style>
